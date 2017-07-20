f6883ba8e161cf68782fdccc13cce3be14d48e53
Shengrui Cen <cenrui.93@gmail.com>
2017-07-20 15:42:01 -0700

    Added first test file

---
+++ src/HelloWorldSol.java
@@ -0,0 +1,11 @@
+/**
+ * Created by arthurcen on 7/20/17.
+ */
+public class HelloWorldSol {
+    static void helloWorld() {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args) {
+        helloWorld();
+    }
+}
